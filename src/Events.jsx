8cdc33b306c678f0cc34705eebdc09839b4683c9
Ujjwal Paul <paulujjwal47@gmail.com>
2024-12-28 21:27:31 +0530

    Working on Events Page

--- src/Events.jsx
+++ src/Events.jsx
@@ -9,12 +9,35 @@ export default function Events() {
                 <img className="h-full w-full object-cover" src="/cover.jpg" alt="community" loading="lazy" />
             </div>
 
-            <div className="px-5 pt-28 pb-44 text-center">
+            <div className="bg-[green] px-5 pt-20 pb-14 text-center">
                 <p className="uppercase text-[#E53637] font-[700] tracking-[2px] mb-10">Workshops & Events</p>
                 <p className="font-[700] text-[2.5rem] leading-[2.5rem]  mb-5">Urban Workshops and Events</p>
                 <p className="text-[#555]">Discover handwoven cloths from artisans across India. Every purchase supports traditional craftsmanship.</p>
             </div>
 
+            <div className="bg-[red]">
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2024/12/13/10/23/woman-9264738_1280.jpg" alt="dress" loading="lazy" />
+                    <div>
+                        <p>Urban Royalty Red</p>
+                        <p>Learn how to drape a cloths confidently for any occasion</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2022/12/04/07/03/woman-7633843_1280.jpg" alt="dress" loading="lazy" />
+                    <div>
+                        <p>Urban Royalty Blue</p>
+                        <p>Explore the history and craftsmanship of cloths from different regions</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2021/03/22/16/07/woman-6115105_1280.jpg" alt="dress" loading="lazy" />
+                    <div>
+                        <p>Urban Royalty Sky</p>
+                        <p>Modern styling hacks to incorporate sarees into your wardrobe</p>
+                    </div>
+                </div>
+            </div>
 
 
 
@@ -37,9 +60,9 @@ export default function Events() {
                 </form>
             </div>
 
-            <div className="bg-[#eee] flex flex-col">
-                <p className="bg-[green] md:w-1/2">“The saree isn’t just an outfit; it’s a story woven into six yards of elegance. Whether you’re a saree veteran or someone curious about its charm, you’re in the right place.”</p>
-                <img className="md:w-1/2 h-[400px] object-cover" src="https://cdn.pixabay.com/photo/2017/08/06/22/52/blouse-2597205_640.jpg" alt="cloth img" loading="lazy" />
+            <div className="bg-[#eee] flex flex-col lg:flex-row">
+                <p className="lg:w-1/2 flex items-center text-center py-24 px-12">“The saree isn’t just an outfit; it’s a story woven into six yards of elegance. Whether you’re a saree veteran or someone curious about its charm, you’re in the right place.”</p>
+                <img className="lg:w-1/2 h-[400px] object-cover" src="https://cdn.pixabay.com/photo/2017/08/06/22/52/blouse-2597205_640.jpg" alt="cloth img" loading="lazy" />
             </div>
 
         </div>
