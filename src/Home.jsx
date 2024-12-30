e16507fb874f9640b98492d3eb421d91fdeb16b2
Ujjwal Paul <paulujjwal47@gmail.com>
2024-12-30 18:00:56 +0530

    Working on Home Page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 
 import { FaRegCalendarCheck } from "react-icons/fa6";
 
+import NumberTicker from "./utils/NumberTicker";
+import Button from "./Button";
+import { NavLink } from "react-router-dom";
+
 
 
 export default function Home() {
@@ -9,6 +13,49 @@ export default function Home() {
 
 
 
+            <div className="bg-[#eee] py-32">
+                <div className="px-5 max-w-[1200px] m-auto">
+                    <p className="text-[1.5rem] mb-4">The cloths isn’t just an outfit; it’s a story woven into six yards of elegance. Whether you’re a cloths veteran or someone curious about its charm, you’re in the right place.</p>
+
+                    <NavLink to="about">
+                        <Button message="Explore More" />
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="w-[85vw] max-w-[1100px] m-auto flex flex-wrap justify-between mt-28 mb-20">
+                <div className="flex items-center mr-5">
+                    <p className="font-[700] text-[4rem] mr-1"> <NumberTicker value={35} /> </p>
+                    <p className="font-[700] text-[1.2rem] leading-[1.5rem] text-[#333] h-fit">Total <br /> Categories</p>
+                </div>
+                <div className="flex items-center mr-5">
+                    <p className="font-[700] text-[4rem] mr-1"> <NumberTicker value={12} /> </p>
+                    <p className="font-[700] text-[1.2rem] leading-[1.5rem] text-[#333] h-fit">Total <br /> Countries</p>
+                </div>
+                <div className="flex items-center">
+                    <p className="font-[700] text-[4rem] mr-1"> <NumberTicker value={97} />% </p>
+                    <p className="font-[700] text-[1.2rem] leading-[1.5rem] text-[#333] h-fit">Happy <br /> Customers</p>
+                </div>
+            </div>
+
+            <div className="px-5 pt-5 pb-10 text-center max-w-[1200px] m-auto border-t border-t-[#00000030]">
+                <p className="uppercase text-[#E53637] font-[700] tracking-[2px] mb-5">Shops</p>
+                <p className="font-[700] text-[2.5rem] leading-[2.5rem] mb-2">Support Local Artisans</p>
+                <p className="text-[#555]">Discover handwoven cloths from artisans across India. Every purchase supports traditional craftsmanship.</p>
+            </div>
+
+
+
+            <div className="bg-[red]">
+                images will be here
+            </div>
+
+
+
+            <div className="px-5 pt-20 pb-10 text-center">
+                <p className="uppercase text-[#E53637] font-[700] tracking-[2px] mb-5">Latest News</p>
+                <p className="font-[700] text-[2.5rem] leading-[2.5rem]">Fashion New Trends</p>
+            </div>
 
             <div className="px-5 mb-24">
                 <div className="flex flex-wrap sm:w-[600px] lg:w-full max-w-[1200px] m-auto">
